673e42f3513192377394c3111971471e80c9c877
Mattison chao <mattisonchao@gmail.com>
2020-11-21 10:01:00 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.70"
+}
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
